8e2c6c37898a9d7b5b97ec0ba72a8be8ce90aa0e
mengyu <mengyu@xiaomi.com>
2017-10-31 18:27:58 +0800

    增加例子服务器

---
+++ SimpleServer/SimpleServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using log4net;
+using System.Reflection;
+using SimpleServer.Services;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace SimpleServer
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
